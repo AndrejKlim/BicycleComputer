67b903648dad6ce697e27ab4a859f866d20058dd
Andrei Klimovich <tydysch@mail.ru>
2021-04-14 20:55:23 +0300

    Added simple logic for handling Hall activating

--- velo_comp.ino
+++ velo_comp.ino
@@ -1,7 +1,33 @@
-void setup() {
+#define HallSensor 2
 
+int prevHallValue;
+uint32_t startTime = millis();
+uint32_t previousTime;
+uint32_t currentTime;
+
+void setup() {
+    startTime = millis();
+    pinMode(HallSensor, INPUT);
+    Serial.begin(9600);
+    previousTime = currentTime;
+    prevHallValue = 1;
 }
 
-void loop() {
+void loop() {    
+    if (isHallActivated(prevHallValue)){
+        currentTime = millis();
+        uint32_t rollTime = currentTime - previousTime;
+        previousTime = millis();
+        Serial.println("  Время на один оборот " + (String)rollTime);
+    }
+    prevHallValue = checkHall();
+    delay(10);
+}
 
+bool isHallActivated(int prevHallValue){
+    return (prevHallValue == 1 and checkHall() == 0);
+}
+
+int checkHall(){
+  return digitalRead(HallSensor);
 }
